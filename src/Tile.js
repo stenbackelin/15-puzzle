0a3bc6de024f185f63012755affda4ec10d8761e
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-26 20:54:05 +0200

    sort in random ordern

--- src/Tile.js
+++ src/Tile.js
@@ -1,6 +1,6 @@
 function Tile() {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    numbers.sort();
+    numbers.sort(() => 0.5 - Math.random());
 
     return (
         numbers.map(number =>

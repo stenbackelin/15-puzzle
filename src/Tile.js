6ec63655354e9663c13d75339270e212e2876402
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-26 19:55:52 +0200

    basic styling and creating list of tiles

--- src/Tile.js
+++ src/Tile.js
@@ -1,7 +1,11 @@
 function Tile() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const listItems = numbers.map(number => <div className="tile">{number}</div>);
+
     return (
-      <div className="tile">
-      </div>
+        <>
+        {listItems}
+        </>
     );
 }
   

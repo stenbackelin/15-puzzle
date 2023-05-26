88936ff398e214e5790f8d72b732f8f02b44a043
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-26 20:50:00 +0200

    sorting array and giving items key and id

--- src/Tile.js
+++ src/Tile.js
@@ -1,11 +1,17 @@
 function Tile() {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    const listItems = numbers.map(number => <div className="tile">{number}</div>);
+    numbers.sort();
 
     return (
-        <>
-        {listItems}
-        </>
+        numbers.map(number =>
+            <div 
+            key={number.toString()}
+            id={number.toString()}
+            className="tile"
+            >
+                {number}
+            </div>    
+        )
     );
 }
   

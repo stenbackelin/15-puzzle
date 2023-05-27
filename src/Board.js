6562227dcc776e832a6ff7218d91bdd1fd1496b9
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-27 12:01:59 +0200

    adding button and styling

--- src/Board.js
+++ src/Board.js
@@ -2,9 +2,12 @@ import Tile from './Tile.js';
 
 function Board() {
     return (
-      <div className="board">
-        <Tile />
-      </div>
+        <>
+        <div className="board">
+            <Tile />
+        </div>
+        <button className='shuffle-btn'>Slumpa</button>
+        </>
     );
 }
   
--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,10 @@
 function Tile() {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].sort(() => 0.5 - Math.random()); // Creating array and sorting it randomly
     const number = numbers.map((x,i) => ({value : x, index : i})); // Mapping array to set value and index
-    
 
-    console.log(number);
+    //const orderedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    //const solved = JSON.stringify(orderedNumbers) === JSON.stringify(numbers)
+    //console.log(solved)
 
     return (
         number.map((x,i) =>
@@ -12,7 +13,7 @@ function Tile() {
             id={i}
             className={`tile ${x.value === 15 ? 'disabled' : ''}`} // Value = 15 ? give it class disabled
             >
-                {x.value === 15 ? '' : x.value /* Value = 15 ? don't print */}
+                {x.value === 15 ? '' : x.value /* Value = 15 ? don't show */}
             </div>    
         )
     );

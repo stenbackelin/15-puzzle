1b5ab36f7cfcc6695eb96d2881f147dd015752e6
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-27 08:04:16 +0200

    set value and index to numbers, don't show 15

--- src/Tile.js
+++ src/Tile.js
@@ -1,15 +1,18 @@
 function Tile() {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    numbers.sort(() => 0.5 - Math.random());
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].sort(() => 0.5 - Math.random()); // Creating array and sorting it randomly
+    const number = numbers.map((x,i) => ({value : x, index : i})); // Mapping array to set value and index
+    
+
+    console.log(number);
 
     return (
-        numbers.map(number =>
+        number.map((x,i) =>
             <div 
-            key={number.toString()}
-            id={number.toString()}
-            className="tile"
+            key={i}
+            id={i}
+            className={`tile ${x.value === 15 ? 'disabled' : ''}`} // Value = 15 ? give it class disabled
             >
-                {number}
+                {x.value === 15 ? '' : x.value /* Value = 15 ? don't print */}
             </div>    
         )
     );

fb2c719e185bf7659a521c2a280fd2f9ae41cc37
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-29 10:19:11 +0200

    moving functions to board - made sense

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,29 @@
 import Tile from './Tile.js';
+import Message from './Message.js';
+import Button from './Button.js';
+import { useState } from "react";
 
 function Board() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].sort(shuffle); // creating array and calling on function shuffle
+    const number = numbers.map((x,i) => ({value : x, index : i})); // mapping array to set value and index
+    const [state, setState] = useState();
+    const reset = () => setState(shuffle()); // reset sets a new state with the function shuffle
+
+    // function returns shuffled array
+    function shuffle() {
+        return (
+            0.5 - Math.random()
+        )
+    }
+
     return (
-        <Tile />
+        <>
+        <Message number={number} />
+        <div className="board">
+            <Tile number={number} />
+        </div>
+        <Button reset={reset} />
+        </>
     );
 }
   
--- src/Tile.js
+++ src/Tile.js
@@ -1,19 +1,5 @@
-import { useState } from "react";
-import Button from "./Button";
-import Message from "./Message";
-
-function Tile() {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].sort(shuffle); // Creating array and calling on function shuffle
-    const number = numbers.map((x,i) => ({value : x, index : i})); // Mapping array to set value and index
-
-    // function returns shuffled array
-    function shuffle() {
-        return (
-            0.5 - Math.random()
-        )
-    }
-
-    const emptyTile = number.find(n => n.value === 15).index; // Finding empty tile and getting index
+function Tile({number}) {
+    const emptyTile = number.find(n => n.value === 15).index; // finding empty tile and getting index
 
     function swapTile(event, i) {
         if(i === emptyTile -1 || i === emptyTile +1 || i === emptyTile -5 || i === emptyTile +5) {
@@ -22,26 +8,17 @@ function Tile() {
             console.log('inte vid sida')
         )
     }
-
-    const [state, setState] = useState();
-    const reset = () => setState(shuffle()); // reset sets a new state with the function shuffle
-
+    
     return (
-        <>
-        <Message number={number} />
-        <div className="board">
-            {number.map((x,i) =>
-                <div 
-                key={i}
-                className={`tile ${x.value === 15 ? 'disabled' : ''}`} // Value = 15 ? give it class disabled
-                onClick={event => swapTile(event, i)}
-                >
-                    {x.value === 15 ? '' : x.value /* Value = 15 ? don't show */}
-                </div>    
-            )}
-        </div>
-        <Button reset={reset} />
-        </>
+        number.map((x,i) =>
+            <div 
+            key={i}
+            className={`tile ${x.value === 15 ? 'disabled' : ''}`} // value = 15 ? give it class disabled
+            onClick={event => swapTile(event, i)}
+            >
+                {x.value === 15 ? '' : x.value /* value = 15 ? don't show */}
+            </div>    
+        )
     );
 }
   

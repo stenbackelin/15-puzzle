d79b5e7e460be64bc522685fae54588d43745378
seodr-elin <128522855+seodr-elin@users.noreply.github.com>
2023-05-30 09:49:22 +0200

    app info to readme

--- src/Tile.js
+++ src/Tile.js
@@ -1,8 +1,9 @@
 function Tile({number}) {
     const emptyTile = number.find(n => n.value === 15).index; // finding empty tile and getting index
 
-    function swapTile(event, i) {
+    function swapTile(event, i, x) {
         if(i === emptyTile -1 || i === emptyTile +1 || i === emptyTile -5 || i === emptyTile +5) {
+            console.log('Innehåller:', x.value, 'Index:', i);
             console.log('vid sidan av')
         } else (
             console.log('inte vid sida')
@@ -14,7 +15,7 @@ function Tile({number}) {
             <div 
             key={i}
             className={`tile ${x.value === 15 ? 'disabled' : ''}`} // value = 15 ? give it class disabled
-            onClick={event => swapTile(event, i)}
+            onClick={event => swapTile(event, i,x)}
             >
                 {x.value === 15 ? '' : x.value /* value = 15 ? don't show */}
             </div>    
